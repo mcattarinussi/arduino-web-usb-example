cd65addcdd09313f6815a33e0d500db846e861f7
Mattia Cattarinussi <mcattarinussi@gmail.com>
2020-05-01 15:37:08 +0200

    Updates

---
+++ precache-manifest.d73ef7c4800dbf2aa602c2e822f47656.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "724400dd5beebdb114c075170f612d15",
+    "url": "/arduino-web-usb-example/index.html"
+  },
+  {
+    "revision": "3da2a5faf538a193935b",
+    "url": "/arduino-web-usb-example/static/js/2.40249d23.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/arduino-web-usb-example/static/js/2.40249d23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ce75ed8b0520a2b19d5",
+    "url": "/arduino-web-usb-example/static/js/main.542b97bf.chunk.js"
+  },
+  {
+    "revision": "f1fbc31903a1fd4bf0d4",
+    "url": "/arduino-web-usb-example/static/js/runtime-main.da64698a.js"
+  }
+]);
